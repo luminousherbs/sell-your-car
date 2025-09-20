9d3ceb13bf000559f4b3023fbe60df939abea771
luminousherbs <172189078+luminousherbs@users.noreply.github.com>
2025-09-20 21:34:42 +0100

    Create serve.js

---
+++ serve.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const fs = require("node:fs");
+const app = express();
+
+const port = 8000;
+
+app.use(express.json()); // parse json
+app.use(express.static(path.join(__dirname, "/"))); // serve from root
+
+app.listen(port, () => {
+    console.log(`Serving on ${port}`);
+});
